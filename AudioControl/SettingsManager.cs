7cda02e5ecc7e2c109c938f7c99985b1570e93c5
Dungeoner <dungeonerr@gmail.com>
2023-06-30 19:59:30 +0300

    added SettingsManager and settingsModel for saving chasen device into file

---
+++ AudioControl/SettingsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AudioControl
+{
+	internal class SettingsManager
+	{
+		private const string SettingsFilePath = "settings.json";
+		internal bool IsSavedExists()
+		{
+			return File.Exists(SettingsFilePath);
+		}
+		internal SettingsModel ReadSettings()
+		{
+			var content = File.ReadAllText(SettingsFilePath);
+			return JsonSerializer.Deserialize<SettingsModel>(content)?? new SettingsModel();
+		}
+		internal void WriteSettings(SettingsModel settingsModel)
+		{
+			string content = JsonSerializer.Serialize(settingsModel);
+			File.WriteAllText(SettingsFilePath, content);
+		}
+	}
+}
---
+++ AudioControl/SettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioControl
+{
+	public class SettingsModel
+	{
+		public string DeviceName { get; set; }
+		public float GainLevel { get; set; }
+	}
+}
