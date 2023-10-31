aa940e901013e22a1c65aa58800a47095ef73d6f
dungeoner <dungeonerr@gmail.com>
2023-10-31 21:30:05 +0200

    moved API to separated proj

---
+++ AudioDeviceManager/Intefaces/IAudioDevice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioControl.Intefaces
+{
+    internal interface IAudioDevice
+    {
+        string Name { get; }
+        int Gain { get; set; }
+    }
+}
