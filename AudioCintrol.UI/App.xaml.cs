49d9edccc2bea02dd61d1329395c08dd701fd6ae
dungeoner <dungeonerr@gmail.com>
2023-11-28 23:05:59 +0200

    Removed console UI, added WinUi3

---
+++ AudioCintrol.UI/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using AudioControl.Intefaces;
+using Castle.Windsor;
+using Microsoft.UI.Xaml;
+using AudioDeviceManager;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AudioControl.UI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            var container = new WindsorContainer();
+            container.Register(Castle.MicroKernel.Registration.Component.For<MainWindow>());
+            container.Register(Castle.MicroKernel.Registration.Component.For<IAudioDeviceManager>().ImplementedBy<DeviceManager>());
+            m_window = container.Resolve<MainWindow>();
+            m_window.Activate();
+        }
+
+        private Window m_window;
+    }
+}
