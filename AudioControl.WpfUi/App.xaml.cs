44f2f23f64fe95e18ddfde8df15c0780c6210077
dungeoner <dungeonerr@gmail.com>
2024-02-05 05:24:51 +0200

    Added WPF project + winforms fot notifyIcon

---
+++ AudioControl.WpfUi/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace AudioControl.WpfUi
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Forms.NotifyIcon notifyIcon = new Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("Assets/pci-card-sound.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+            notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+            this.MainWindow = new MainWindow();
+            this.MainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        private void NotifyIcon_DoubleClick(object? sender, System.EventArgs e)
+        {
+            if(this.MainWindow != null && !this.MainWindow.IsVisible)
+            {
+                this.MainWindow.Show();
+            }
+        }
+    }
+}
