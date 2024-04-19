53bddf8ec626aee6f9554bab637b693b6de6eae9
dungeoner <dungeonerr@gmail.com>
2024-04-20 02:19:38 +0300

    Buttons revork

---
+++ AudioControl.WpfUi/Core/Controls/IconButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AudioControl.WpfUi.Core.Controls
+{
+    public class IconButton : Button
+    {
+        static IconButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IconButton), new FrameworkPropertyMetadata(typeof(IconButton)));
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon),
+                                                typeof(Geometry),
+                                                typeof(IconButton));
+
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+    }
+}
---
+++ AudioControl.WpfUi/Core/Controls/RoundedButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace AudioControl.WpfUi.Core.Controls
+{
+    public class RoundedButton : IconButton
+    {
+        static RoundedButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundedButton),
+               new FrameworkPropertyMetadata(typeof(RoundedButton)));
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius),
+                                                typeof(CornerRadius),
+                                                typeof(RoundedButton));
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+    }
+}
